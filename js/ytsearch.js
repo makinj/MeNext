92387fe67208b3ec153403f5df3e1ce9ad625b63
Joshua Makinen <makinj64@gmail.com>
2014-03-24 21:56:49 -0400

    takes away depency on Zend(for now) and moves submission searching to client side (Jim setup Android with it, for instructions remind me to give them to you)

--- js/ytsearch.js
+++ js/ytsearch.js
@@ -3,6 +3,7 @@ function searchVideo(){
   Joshua Makinen
   Calls AJAX to retreive search result
   */
+  var API_KEY = 'AIzaSyCIDavzeJA_rfK90XD3O2o5JRyIMOFyUvM';
   var search_term=document.getElementById("search").value;
   if (search_term.length==0){
     document.getElementById("list").innerHTML="<h5>Enter video to search</h5>";
@@ -12,13 +13,16 @@ function searchVideo(){
     xmlhttp.onreadystatechange=function(){
       if (xmlhttp.readyState==4 && xmlhttp.status==200){
         document.getElementById("list").innerHTML="";
-        var videos= JSON.parse(xmlhttp.responseText)
+        alert(xmlhttp.responseText);
+        
+        var videos= JSON.parse(xmlhttp.responseText).items;
+        
         for (var i=0;i<videos.length;i++){
           //var txt = document.createTextNode("<a href='https://www.youtube.com/watch?v="+videos[i][1]+"'><li>"+i+"<img src='"+videos[i][2]+"'/>"+videos[i][0]+"</li></a>");
           
           var link=document.createElement("LI");
           //var position=i+1;
-          link.innerHTML=i+"<a href='https://www.youtube.com/watch?v="+videos[i][1]+"'>"+"<img src='"+videos[i][2]+"'/>"+videos[i][0]+"</a>";
+          link.innerHTML=(i+1).toString()+"<a href='https://www.youtube.com/watch?v="+videos[i].id.videoId+"'>"+"<img src='"+videos[i].snippet.thumbnails.default.url+"'/>"+videos[i].snippet.title+"</a>";
 //document.body.appendChild(link);
 
 
@@ -27,11 +31,16 @@ function searchVideo(){
           //document.write(videos[i] + "<br>");
         }
         //document.getElementById("list").innerHTML=xmlhttp.responseText;
+        
+        //document.getElementById("list").innerHTML="done";
+
       }else{
         document.getElementById("list").innerHTML="Loading...";
       }
     }
-    xmlhttp.open("GET","includes/ytfunctions.php?action=search&search="+search_term,true);
+    xmlhttp.open("GET","https://www.googleapis.com/youtube/v3/search?part=snippet&order=relevance&maxResults=25&q="+search_term+"&key="+API_KEY,true);
+
+    //xmlhttp.open("GET","includes/ytfunctions.php?action=search&search="+search_term,true);
     xmlhttp.send();
   }
   return false;
