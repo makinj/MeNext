dac89f26ae684f61070d3268b45df6e4ce19aca1
Surgens <watterspm@gmail.com>
2014-03-24 20:11:30 -0400

    Design rough draft of initial database setup.

--- class.DB.php
+++ class.DB.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class DB
 {
   private $_db;
@@ -21,8 +20,6 @@ class DB
   }
 
 
-
-
   private function setup(){
     require("includes/constants.php");
     try {
@@ -38,15 +35,52 @@ class DB
       exit;
     }
     $this->_db = new PDO("mysql:host=$DB_HOST;dbname=$DB_NAME", $DB_USER, $DB_PASS); 
-    $this->createTable('USERS', 'uid int NOT NULL AUTO_INCREMENT, user VARCHAR(16), pass VARCHAR(128), admin BIT(1), INDEX(user(6)), PRIMARY KEY(uid)');
-    $this->createTable('SUBMISSIONS', 'sid int NOT NULL AUTO_INCREMENT, uid int, ytid VARCHAR(11), played BIT(1), INDEX(uid,sid), PRIMARY KEY(sid)');
+    // Temporarily not enforcing foreign key constraints, only noting with "References"
+    $this->createTable('User',
+    'userId int NOT NULL AUTO_INCREMENT,
+     username VARCHAR(16) UNIQUE,
+     password VARCHAR(128),
+     admin BIT(1) DEFAULT 0,
+     date_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     INDEX(username),
+     PRIMARY KEY(userId)');
+    $this->createTable('Video', 
+    'videoId int NOT NULL AUTO_INCREMENT, 
+     submitterId int REFERENCES User(userId),
+     ytid VARCHAR(11), 
+     title VARCHAR(255), 
+     length INT, 
+     url VARCHAR(255), 
+     played BIT(1) DEFAULT 0, 
+     PRIMARY KEY(videoId), 
+     INDEX(submitterId, title)');
+    $this->createTable('VideoParty',
+    'vpid int NOT NULL AUTO_INCREMENT,
+     title VARCHAR(255),
+     creatorId int REFERENCES User(uid),
+     PRIMARY KEY(vpid)');
+
+    // This table links videos to video parties. This way, we don't need
+    // to store the same video's information more than once if it's played
+    // several times across several video parties.
+    // NOTE: duplicate (videoId, vpId) tuples will be allowed to
+    //   exist so the same song can be submitted to a single video
+    //   video party twice.
+    // Index is on videoPartyId, then rating. This will facilitate searching
+    // a certain video party for the highest rated song.
+    $this->createTable('Video_VideoParty',
+    'videoId int REFERENCES Video(videoId),
+     videoPartyId int REFERENCES VideoParty(vpid),
+     upvotes int,
+     downvotes int,
+     rating int,
+     wasPlayed BIT(1) DEFAULT 0,
+     INDEX(videoPartyId, wasPlayed, rating) ');
     $this->createAccount($ADMIN_NAME, $ADMIN_PASS, 1);
   }
 
-
   private function createTable($name, $query){
     try{
-
       $this->_db->exec("create table $name($query);");
     }catch(PDOException $e){
       echo 'Connection failed: ' . $e->getMessage()."<br>";
@@ -57,7 +91,7 @@ class DB
     require_once("includes/functions.php");
     require("includes/constants.php");
     $name = sanitizeString($name);
-    $result = $this->_db->prepare("SELECT * FROM USERS WHERE user=:username;");
+    $result = $this->_db->prepare("SELECT * FROM User WHERE username=:username;");
     $result->bindValue(':username', $name);
     $result->execute();
     return ($result->rowCount()>0);
@@ -72,7 +106,7 @@ class DB
     if($this->isUser($username)){
       echo "player $username already exists";
     } else {
-      $stmt = $this->_db->prepare("insert into USERS (user, pass, admin) VALUES(:username, :password, :admin);");
+      $stmt = $this->_db->prepare("insert into User (username, password, admin) VALUES(:username, :password, :admin);");
       $stmt->bindValue(':username', $username);
       $stmt->bindValue(':password', $password);
       $stmt->bindValue(':admin', $admin, PDO::PARAM_BOOL);
@@ -84,7 +118,7 @@ class DB
     require("includes/constants.php");
     $username = sanitizeString($username);
     $password = hash('sha512',$pre_salt.sanitizeString($password).$post_salt);
-    $stmt = $this->_db->prepare("SELECT * FROM USERS WHERE user=:username and pass=:password;");
+    $stmt = $this->_db->prepare("SELECT * FROM User WHERE username=:username and password=:password;");
     $stmt->bindValue(':username', $username);
     $stmt->bindValue(':password', $password);
     $stmt->execute();
@@ -105,12 +139,15 @@ class DB
     }
     
   }
+
+  // Fields for song include:
+  //  Title, Length, url (ytid??), submitterId (uid??)
   public function addSong($uid, $ytid){
     require_once("includes/functions.php");
     require("includes/constants.php");
     $uid = sanitizeString($uid);
     $ytid = sanitizeString($ytid);
-    $stmt = $this->_db->prepare("insert into SUBMISSIONS (uid, ytid) VALUES(:uid, :ytid);");
+    $stmt = $this->_db->prepare("INSERT INTO Video (uid, ytid) VALUES(:uid, :ytid);");
     $stmt->bindValue(':uid', $uid);
     $stmt->bindValue(':ytid', $ytid);
     $stmt->execute();
@@ -121,4 +158,4 @@ class DB
   }
 }
 
-?>
+?> 
