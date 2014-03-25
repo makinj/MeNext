5461249a923bd1f8fdb5936496d17af430d606c2
Joshua Makinen <makinj64@gmail.com>
2014-03-24 22:17:14 -0400

    cleans up some youtube searching

--- includes/constants.php
+++ includes/constants.php
@@ -7,4 +7,5 @@
   $ADMIN_PASS = 'password';//admin password name
   $pre_salt = "5W4GG#d0pe&&*";//for security(be random)
   $post_salt = "UILU#do114b111zzz@#)";//see above
+  $API_CLIENT_KEY = "AIzaSyCIDavzeJA_rfK90XD3O2o5JRyIMOFyUvM"; 
 ?>
--- submit.php
+++ submit.php
@@ -8,6 +8,13 @@
     <input type="text" name="search" id="search"/>
     <input type="submit" name="submit" value="go!"/>
 </form>
+<script type="text/javascript">
+  var API_KEY =
+  <?php 
+    require_once("includes/constants.php");
+    echo"'".$API_CLIENT_KEY."'";
+  ?>;
+</script>
 <script type="text/javascript" src="/js/ytsearch.js"></script>
 <ul id="list"></ul>
 <?php
