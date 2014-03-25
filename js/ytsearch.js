b8a99d793b9488e1ab736cf10df04d0a73a54ff0
Joshua Makinen <makinj64@gmail.com>
2014-03-25 00:31:44 -0400

    even nicer searching (remove old code and the search button) makes it neater

--- js/ytsearch.js
+++ js/ytsearch.js
@@ -1,64 +1,33 @@
-function searchVideo(){
-  /*
-  Joshua Makinen
-  Calls AJAX to retreive search result
-  */
-  var search_term=document.getElementById("search").value;
-  if (search_term.length==0){
-    document.getElementById("list").innerHTML="<h5>Enter video to search</h5>";
-    return false;
-  }else{
-    var xmlhttp=new XMLHttpRequest();
-    xmlhttp.onreadystatechange=function(){
-      if (xmlhttp.readyState==4 && xmlhttp.status==200){
-        document.getElementById("list").innerHTML="";       
-        var videos= JSON.parse(xmlhttp.responseText).items;
-        for (var i=0;i<videos.length;i++){
-          var link=document.createElement("LI");
-          link.innerHTML=(i+1).toString()+"<a href='https://www.youtube.com/watch?v="+videos[i].id.videoId+"'>"+"<img src='"+videos[i].snippet.thumbnails.default.url+"'/>"+videos[i].snippet.title+"</a>";
-          document.getElementById('list').appendChild(link); 
-        }
-      }else{
-        document.getElementById("list").innerHTML="Loading...";
-      }
-    }
-    xmlhttp.open("GET","https://www.googleapis.com/youtube/v3/search"+
-      "?part=snippet"+//don't question this part
-      "&order=relevance"+//sort by relevance
-      "&maxResults=25"+//up to 25 results
-      "&q="+search_term+//query with search term
-      "&key="+API_KEY,//Client API key
-      true
-    );
-
-    xmlhttp.send();
-  }
-  return false;
-}
-
+/*
+Joshua Makinen
+Calls AJAX to retreive search result
+*/
 $(document).ready(function(){
   $("#search").keyup(function(){
-    $.get("https://www.googleapis.com/youtube/v3/search"+
-      "?part=snippet"+//don't question this part
-      "&order=relevance"+//sort by relevance
-      "&type=video"+//only show videos(not channels or playlists)
-      "&maxResults=25"+//up to 25 results
-      "&q="+$(this).val()+//query with search term
-      "&key="+API_KEY,//Client API key
-      
-      function(data,status){
-        if (status=="success"){
-          //alert(data);       
-          document.getElementById("searchResults").innerHTML="";
-          var videos= data.items;
-          for (var i=0;i<videos.length;i++){
-            var link=document.createElement("LI");
-            link.innerHTML=(i+1).toString()+"<a href='https://www.youtube.com/watch?v="+videos[i].id.videoId+"'>"+"<img src='"+videos[i].snippet.thumbnails.default.url+"'/>"+videos[i].snippet.title+"</a>";
-            document.getElementById('searchResults').appendChild(link); 
+    if($(this).val()!=""){
+      $.get("https://www.googleapis.com/youtube/v3/search"+
+        "?part=snippet"+//don't question this part
+        "&order=relevance"+//sort by relevance
+        "&type=video"+//only show videos(not channels or playlists)
+        "&maxResults=25"+//up to 25 results
+        "&q="+$(this).val()+//query with search term
+        "&key="+API_KEY,//Client API key
+        
+        function(data,status){
+          if (status=="success"){
+            document.getElementById("searchResults").innerHTML="";
+            var videos= data.items;
+            for (var i=0;i<videos.length;i++){
+              var link=document.createElement("LI");
+              link.innerHTML=(i+1).toString()+"<a href='https://www.youtube.com/watch?v="+videos[i].id.videoId+"' target='_blank'>"+"<img src='"+videos[i].snippet.thumbnails.default.url+"'/>"+videos[i].snippet.title+"</a>";
+              document.getElementById('searchResults').appendChild(link); 
+            }
+          }else{
+            document.getElementById("searchResults").innerHTML="Failed :(";
           }
-        }else{
-          document.getElementById("searchResults").innerHTML="Failed :(";
-        }
-    });
+      });
+    }else{
+      document.getElementById("searchResults").innerHTML="";
+    }
   });
 });
--- submit.php
+++ submit.php
@@ -3,11 +3,9 @@
   $title="submit";
   require_once('header.php');//echo top of html
 ?>
-
-<form action="" name="contact" onsubmit="return searchVideo();">
-    <input type="text" name="search" id="search"/>
-    <input type="submit" name="submit" value="go!"/>
-</form>
+<h3>Search for videos here: 
+  <input type="text" name="search" id="search"/>
+</h3>
 <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 <script type="text/javascript">
   var API_KEY =
