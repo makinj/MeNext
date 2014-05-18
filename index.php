9c5c1523d26a9106c8cf6724d49e51f4adb4246c
makinj <basicislife@gmail.com>
2014-05-18 03:41:24 -0400

    moves script tags to the footer as they should have been from the beginning, also adds tables for the display of queues and search results, also makes the queue update every 5 seconds

--- index.php
+++ index.php
@@ -2,7 +2,21 @@
   $title="index";
   require_once('header.php');//bar at the top of the page
 ?>
-  <ul id="videoList"></ul>
+  <div class="panel panel-default">
+    <div class="panel-heading">Song Queue</div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Title</th>
+        </tr>
+      </thead>
+      <tbody id="queueList">
+      </tbody>
+      
+    </table>
+  </div>
+
   <a href='reset.php'>reset</a>
 <?php
   require_once('footer.php');//bar at the top of the page
--- submit.php
+++ submit.php
@@ -5,20 +5,24 @@
   require_once('header.php');//echo top of html
 ?>
 
-<ul id="searchResults"></ul>
+<div class="panel panel-default">
+  <div class="panel-heading">Search Results</div>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Add</th>
+        <th>Thumbnail</th>
+        <th>Title</th>
+      </tr>
+    </thead>
+    <tbody id="searchResults">
+    </tbody>
+    
+  </table>
+</div>
 
 
-<script type="text/javascript">
-  var API_KEY =
-  <?php 
-    require_once("includes/constants.php");
-    echo"'".$API_CLIENT_KEY."';\n";
-    if (isset($_GET['q'])){
-      echo "$('#searchText').val('".$_GET['q']."');\n";
-    }
-  ?>
-  searchYouTube();
-</script>
 <script type="text/javascript" src="/js/common.js"></script>
 <?php
   require_once('footer.php');//close up html
