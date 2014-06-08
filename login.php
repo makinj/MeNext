52fbe5eab1da709bb444d05621219c4c097bc7a7
mcaniw <wyler.m.r@gmail.com>
2014-06-08 17:51:44 -0400

    begins work on the login page, introduces purple color

--- login.php
+++ login.php
@@ -1,26 +1,46 @@
 <?php
   require_once("class.DB.php");//database funcitonality 
-  $title="login";//to be displayed in tab
+  $title="Login | Register";//to be displayed in tab
   include("header.php");//open html bar
 ?>
+<table class="login-table">
+  <tr>
+    <td class="reglog-item">
+      <h1 class="login-title">Login</h1>
+      <div id="problem"></div>
+      <form id="login">
+        <div class="form-item">
+          <span class="login-text">Username: </span><input type="text" name="username" id="name">
+        </div>
 
-<h1>login</h1>
-<div id="problem"></div>
-<form id="login">
-  Username: <input type="text" name="username" id="name"><br>
-  Password: <input type="password" name="password" id="password">
-  <input type="submit" name="submit" value="Submit">
-  <input type="hidden" name="action" value='login'>
-</form>
+        <div class="form-item">
+          <span class="login-text">Password: </span><input type="password" name="password" id="password">
+        </div>
 
-<h2>register</h2>
-<form id="register">
-  Username: <input type="text" name="username" id="name"><br>
-  Password: <input type="password" name="password" id="password">
-  <input type="submit" name="submit" value="Submit">
-  <input type="hidden" name="action" value='register'>
-</form>
-<script src="js/common.js"></script>
+        <input type="submit" name="submit" value="Submit" class="submit">
+        
+        <input type="hidden" name="action" value='login'>
+      </form>
+    </td>
+
+    <td class="reglog-item">
+      <h2 class="login-title">Register</h2>
+      <form id="register">
+        <div class="form-item">
+          <span class="login-text">Username: </span><input type="text" name="username" id="name">
+        </div>
+
+        <div class="form-item">
+          <span class="login-text">Password: </span><input type="password" name="password" id="password">
+        </div>
+
+        <input type="submit" name="submit" value="Submit" class="submit">
+
+        <input type="hidden" name="action" value='register'>
+      </form>
+    </td>
+  </tr>
+</table>
 <?php
   require_once("footer.php");//closes html
 ?>
