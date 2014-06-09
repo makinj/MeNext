5a0307e45da2e58fa2b78c9f2a5a2e5803ffcec7
mcaniw <wyler.m.r@gmail.com>
2014-06-09 19:25:40 -0400

    Fixed up naming conventions

--- login.php
+++ login.php
@@ -3,38 +3,42 @@
   $title="Login | Register";//to be displayed in tab
   include("header.php");//open html bar
 ?>
-<table class="login-table">
+<table class="loginTable">
   <tr>
-    <td class="reglog-item">
-      <h1 class="login-title">Login</h1>
+    <td class="registerLoginContainer">
+      <h1 class="loginTitle">Login</h1>
       <div id="problem"></div>
       <form id="login">
-        <div class="form-item">
-          <span class="login-text">Username: </span><input type="text" name="username" id="name">
+        <div class="formItem">
+          <span class="loginText">Username: </span><input type="text" name="username" id="name">
         </div>
 
-        <div class="form-item">
-          <span class="login-text">Password: </span><input type="password" name="password" id="password">
+        <div class="formItem">
+          <span class="loginText">Password: </span><input type="password" name="password" id="password">
         </div>
 
-        <input type="submit" name="submit" value="Submit" class="submit">
-        
+        <div class="formItem">
+          <input type="submit" name="submit" value="Submit" class="submit">
+        </div>
+
         <input type="hidden" name="action" value='login'>
       </form>
     </td>
 
-    <td class="reglog-item">
-      <h2 class="login-title">Register</h2>
+    <td class="registerLoginContainer">
+      <h2 class="loginTitle">Register</h2>
       <form id="register">
-        <div class="form-item">
-          <span class="login-text">Username: </span><input type="text" name="username" id="name">
+        <div class="formItem">
+          <span class="loginText">Username: </span><input type="text" name="username" id="name">
         </div>
 
-        <div class="form-item">
-          <span class="login-text">Password: </span><input type="password" name="password" id="password">
+        <div class="formItem">
+          <span class="loginText">Password: </span><input type="password" name="password" id="password">
         </div>
 
-        <input type="submit" name="submit" value="Submit" class="submit">
+        <div class="formItem">
+          <input type="submit" name="submit" value="Submit" class="submit">
+        </div>
 
         <input type="hidden" name="action" value='register'>
       </form>
