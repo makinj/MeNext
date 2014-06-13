6127584370e76b52ff3f5e3b015d2b17117901f8
mcaniw <wyler.m.r@gmail.com>
2014-06-13 18:00:28 -0400

    Restructures login page and adds new submit button

--- login.php
+++ login.php
@@ -8,40 +8,48 @@
     <td class="registerLoginContainer">
       <h1 class="loginTitle">Login</h1>
       <div id="problem"></div>
-      <form id="login">
-        <div class="formItem">
-          <span class="loginText">Username: </span><input type="text" name="username" id="name">
-        </div>
+      <div class="formContainer">
+        <form id="login">
+          <div class="formItem">
+            <span class="loginText">Username: </span>
+            <input type="text" name="username" id="name">
+          </div>
 
-        <div class="formItem">
-          <span class="loginText">Password: </span><input type="password" name="password" id="password">
-        </div>
+          <div class="formItem">
+            <span class="loginText">Password: </span>
+            <input type="password" name="password" id="password">
+          </div>
 
-        <div class="formItem">
-          <input type="submit" name="submit" value="Submit" class="submit">
-        </div>
+          <div class="formItem">
+            <input type="submit" name="submit" value="Submit" class="submit">
+          </div>
 
-        <input type="hidden" name="action" value='login'>
-      </form>
+          <input type="hidden" name="action" value='login'>
+        </form>
+      </div>
     </td>
 
     <td class="registerLoginContainer">
       <h2 class="loginTitle">Register</h2>
-      <form id="register">
-        <div class="formItem">
-          <span class="loginText">Username: </span><input type="text" name="username" id="name">
-        </div>
+      <div class="formContainer">
+        <form id="register">
+          <div class="formItem">
+            <span class="loginText">Username: </span>
+            <input type="text" name="username" id="name">
+          </div>
 
-        <div class="formItem">
-          <span class="loginText">Password: </span><input type="password" name="password" id="password">
-        </div>
+          <div class="formItem">
+            <span class="loginText">Password: </span>
+            <input type="password" name="password" id="password">
+          </div>
 
-        <div class="formItem">
-          <input type="submit" name="submit" value="Submit" class="submit">
-        </div>
+          <div class="formItem">
+            <input type="submit" name="submit" value="Submit" class="submit">
+          </div>
 
-        <input type="hidden" name="action" value='register'>
-      </form>
+          <input type="hidden" name="action" value='register'>
+        </form>
+      </div>
     </td>
   </tr>
 </table>
