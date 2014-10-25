fb2ef39a31f0e8829b7581bba6038d5135f90c9c
makinj <basicislife@gmail.com>
2014-10-25 19:51:39 +0000

    only redirect if not https

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php
   require_once("includes/functions.php"); //basic database operations
-  if(PRODUCTION){
+  if(PRODUCTION && $_SERVER["HTTPS"] != "on"){
     header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
     exit();
   }
